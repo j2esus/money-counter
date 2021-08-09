693a233f916a0e3bc2f5f4eba1e7fd6730ec6f5c
Jesús Vargas <jesus.vargas@jeegox.com>
2021-08-08 20:59:04 -0500

    Update some details of the project and remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          o sea, hello
+          Hello, world
         </p>
       </header>
     </div>
