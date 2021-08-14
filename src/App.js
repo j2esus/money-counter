a98a0bbf7e8287bf31e691f73d8599e99cd21f14
Jesús Vargas <jesus.vargas@jeegox.com>
2021-08-13 23:49:16 -0500

    First functional version but incomplete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React from 'react';
 import Navbar from './Navbar';
 
 class Item extends React.Component {
-  constructor(props){
-    super(props);
-    this.value = props.value;
+  
+  onChange = event => {
+    const item = {
+      index: this.props.index,
+      value: this.props.value, 
+      quantity: event.target.value,
+    }
+    this.props.multiply(item);
   }
 
   render(){
     return (
-      <div class="card">
-        <div class="card-body">
-          <h4 class="card-title">{this.props.value}</h4>
-          <div class="form-group">
-            <input class="form-control form-control-lg" type="number" min="0" placeholder="Cantidad"/>
+      <div className="card">
+        <div className="card-body">
+          <h4 className="card-title text-end">{this.props.value}</h4>
+          <div className="form-group">
+            <input className="form-control form-control-lg" type="number" min="0" 
+              placeholder="Cantidad"
+              onChange={this.onChange}/>
           </div>
         </div>
       </div>
@@ -22,50 +29,76 @@ class Item extends React.Component {
 }
 
 class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: Array(12).fill(null),
+      result: 0,
+    }
+  }
+
+  renderItem(index, value) {
+    return <Item value={value} multiply={this.multiply} index={index}/>
+  }
+
+  multiply = (item) => {
+    const items = this.state.items.slice();
+    items[item.index] = item.value * item.quantity;
+    const result = items.reduce((a, b) => a + b, 0);
+    this.setState({
+      items: items,
+      result: result,
+    });
+  }
 
   render(){
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-3 mb-2">
-            <Item value="1000"/>
+            {this.renderItem(0, 1000)}
           </div>
           <div className="col-md-3 mb-2">
-            <Item value="500"/>
+            {this.renderItem(1, 500)}
           </div>
           <div className="col-md-3 mb-2">
-            <Item value="200"/>
+            {this.renderItem(2, 200)}
           </div>
           <div className="col-md-3 mb-2">
-            <Item value="100"/>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-md-3 mb-2">
-            <Item value="50"/>
-          </div>
-          <div className="col-md-3 mb-2">
-            <Item value="20"/>
-          </div>
-          <div className="col-md-3 mb-2">
-            <Item value="10"/>
-          </div>
-          <div className="col-md-3 mb-2">
-            <Item value="5"/>
+            {this.renderItem(3, 100)}
           </div>
         </div>
         <div className="row">
           <div className="col-md-3 mb-2">
-            <Item value="2"/>
+            {this.renderItem(4, 50)}
           </div>
           <div className="col-md-3 mb-2">
-            <Item value="1"/>
+            {this.renderItem(5, 20)}
           </div>
           <div className="col-md-3 mb-2">
-            <Item value="0.5"/>
+            {this.renderItem(6, 10)}
           </div>
           <div className="col-md-3 mb-2">
-            <Item value="0.2"/>
+            {this.renderItem(7, 5)}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-3 mb-2">
+            {this.renderItem(8, 2)}
+          </div>
+          <div className="col-md-3 mb-2">
+            {this.renderItem(9, 1)}
+          </div>
+          <div className="col-md-3 mb-2">
+            {this.renderItem(10, 0.5)}
+          </div>
+          <div className="col-md-3 mb-2">
+            {this.renderItem(11, 0.2)}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 mb-2 text-end">
+            <h2>{this.state.result}</h2>
           </div>
         </div>
       </div>
