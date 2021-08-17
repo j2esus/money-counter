27bc6d184c3e899c02bdef85f22b5d04626137e1
Jesús Vargas <jesus.vargas@jeegox.com>
2021-08-16 22:40:18 -0500

    Put components in their own folder/package

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../App';
+import App from '../components/App';
 
 test('validate if the title has the css class', () => {
   render(<App />);
